c48a94b536443ac5e8722f5e7ac3986840fd7068
Alejandro17Cast <luisalejandrocasta3@gmail.com>
2024-05-04 18:17:53 -0600

    InsertionSort

---
+++ App.java
@@ -0,0 +1,34 @@
+public class App {
+
+    public static void InsertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Metodo de Ordenamiento por inserción");
+        int[] arr = { 12, 11, 13, 5, 6 };
+
+        System.out.println("Arreglo original");
+        for (int num : arr) {
+            System.out.println(num + " ");
+        }
+        System.out.println("============================");
+        InsertionSort(arr);
+        System.out.println("Arreglo ordenado");
+        for (int num : arr) {
+            System.out.println(num + " ");
+        }
+
+    }
+}
